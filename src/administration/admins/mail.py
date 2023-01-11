cfc21306f7476547b69e87c900520f281930236b
Ikram Khan <ikram.khan0762@gmail.com>
2023-01-11 18:14:33 +0500

    ADMINS: email verifications added

--- src/administration/admins/mail.py
+++ src/administration/admins/mail.py
@@ -1,5 +1,5 @@
-from django.core.mail import EmailMultiAlternatives, EmailMessage
-from django.template.loader import get_template, render_to_string
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 from core.settings import EMAIL_HOST_USER
 
 
@@ -7,16 +7,21 @@ def sent_email_over_employee_create(recipient):
     try:
         to_list = [recipient.email]
         context = {
+            'org': 'National FM Services',
+            'tagline': 'National FM Services LTD',
+            'link': 'https://nationalfmservices.co,uk',
+            'login_link': 'https://nationalfmservices.thestaffmanager.com/accounts/login/',
             'username': recipient.username,
             'email': recipient.email,
-            'password': '1100@0011' + recipient.username + '0011@0011'
+            'password': '1100@0011' + recipient.username + '0011@0011',
+
         }
         html_message = render_to_string(template_name="emails/employee_registration.html", context=context)
         subject = 'Employee Account Created'
         from_email = EMAIL_HOST_USER
 
         message = EmailMessage(subject, html_message, from_email, to_list)
-        message.content_subtype = 'html'  # this is required because there is no plain text email message
+        message.content_subtype = 'html'
         message.send()
 
     except Exception as e:
