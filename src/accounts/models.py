059611c5feba2ca11167a4179b764a22a212c865
Ikram Khan <ikram.khan0762@gmail.com>
2023-01-13 17:12:22 +0500

    ADMINS: UPDATES: visa passport and many other changes added

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -135,6 +135,10 @@ class Employee(models.Model):
         verbose_name="Country of Birth", related_name='employee_country_of_birth'
     )
 
+    passport_required = models.BooleanField(default=False)
+    passport_country = models.ForeignKey('admins.Country', on_delete=models.SET_NULL, null=True, blank=True)
+    passport_expiry = models.DateField(null=True, blank=True)
+
     # MANY TO MANY
     sites = models.ManyToManyField('admins.Site', blank=True, through='EmployeeSite')
     positions = models.ManyToManyField('admins.Position', blank=True, through='EmployeePosition')
@@ -188,6 +192,7 @@ class EmployeeWork(models.Model):
     ni_number = models.CharField(max_length=255, null=True, blank=True)
     utr = models.CharField(max_length=255, null=True, blank=True)
     tax_code = models.CharField(null=True, blank=True, max_length=255)
+
     visa_required = models.BooleanField(default=False)
     visa_country = models.ForeignKey('admins.Country', null=True, blank=True, on_delete=models.SET_NULL)
     visa_type = models.CharField(max_length=255, null=True, blank=True)
@@ -232,7 +237,7 @@ class EmployeeContract(models.Model):
 class EmployeeQualification(models.Model):
     TYPE_CHOICE = (
         ('fad', 'First AID'),
-        ('sia', 'SIA No'),
+        ('sia', 'SIA License'),
     )
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, blank=True)
     type = models.CharField(max_length=3, choices=TYPE_CHOICE)
